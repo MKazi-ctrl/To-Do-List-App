be1c319851d2393336a5ddacba5c228e7f903ab1
MKazi-ctrl <84854139+MKazi-ctrl@users.noreply.github.com>
2023-11-18 20:21:15 -0500

    Create TaskList.js

---
+++ src/TaskList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Task from './Task';
+
+const TaskList = ({ tasks, onDelete }) => {
+  return (
+    <ul>
+      {tasks.map(task => (
+        <Task key={task.id} task={task} onDelete={onDelete} />
+      ))}
+    </ul>
+  );
+};
+
+export default TaskList;
